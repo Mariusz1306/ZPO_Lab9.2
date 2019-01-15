12b3eb33cca08aad4533ae00f2f780f11baa1db3
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2019-01-15 13:03:39 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@
 
 public class Main {
     public static void main(String args[]){
-        Primes prim = new Primes(20000, 39999);
-        prim.compute();
-        System.out.println(prim.getSafeList());
+        Primes prim = new Primes(20000, 39999); //Stwórz nowy obiekt klasy Primes; (20000, 39999) to przedział w jakim szukamy liczb pierwszych
+        prim.compute(); //Wykonaj obliczenia
+        System.out.println(prim.getSafeList()); //Wypisz wynik
     }
 }
